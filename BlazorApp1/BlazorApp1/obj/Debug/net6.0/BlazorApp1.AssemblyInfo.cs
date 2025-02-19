b44d7b3eab7f921d9bb0d3f63d326f58e4a67bb5
PfennigL <lars.pfennig@efm.at>
2025-02-19 11:17:40 +0100

    test

--- BlazorApp1/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef5c88afde79fdc51505d41867291b6e3973327f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c670a496f8afb0b6828ee52671467cad3e6426")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
